833996df8fb00942c841eb3b555a098cbcf9a9b7
JustGogoing <14536438@qq.com>
2017-05-12 11:16:35 +0800

    qq

--- 测试collectionview/ViewController.m
+++ 测试collectionview/ViewController.m
@@ -56,6 +56,7 @@ static NSString *foot = @"footer";
     
     //造一个数据源
     NSArray *b = @[@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19"];
+    
     NSArray *a = @[@"00",@"01",@"02",@"03",@"04",@"05",@"06",@"07",@"08",@"09"];
 
     upArr = [[NSMutableArray alloc]initWithArray:a];
